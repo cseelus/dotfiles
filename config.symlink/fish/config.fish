8950b136aa39f8d99c6e80febd4bc16d6d53c09f
Chris Seelus <chris@imeos.com>
2014-10-22 21:55:34 +0200

    Fish: Cleanup fishconfig

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -7,7 +7,7 @@ set fish_theme lanai
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # set fish_plugins autojump bundler set git rails
-set fish_plugins set autojump git rails set vi-mode
+set fish_plugins set autojump git rails vi-mode
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
