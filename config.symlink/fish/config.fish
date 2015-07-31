fe731b660d4625707b4214e4dd947a60c2603c5c
Chris Seelus <chris@imeos.com>
2015-07-31 23:48:24 +0200

    Fish: Change rbenv path; Add preview_markdown function

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -1,13 +1,13 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme (good ones are clearance, idan, lanai, numist)
+# Theme (good ones are clearance, idan, lanai, numist, zen)
 set fish_theme zen
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # set fish_plugins autojump bundler git rails vi-mode
-set fish_plugins autojump bundler git rails rake
+set fish_plugins autojump git rails rake
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
@@ -24,10 +24,14 @@ set homebrew /usr/local/bin /usr/local/sbin
 set latex "/usr/texbin"
 set odbcsys "/etc"
 set odbc "/usr/local/etc"
+set brew_rbenv "/usr/local/var/rbenv/shims"
 # PATH ($default should be last)
-set -gx PATH $homebrew $latex $odbcsys $odbc $default_path
+set -gx PATH $homebrew $latex $odbcsys $odbc $brew_rbenv $default_path
 # No more bundle exec
 # set -gx  RUBYGEMS_GEMDEPS -
+
+# Ruby (rbenv)
+set -gx RBENV_ROOT /usr/local/var/rbenv
 
 # Aliases
 alias bex="bundle exec"
@@ -40,6 +44,7 @@ alias vi="mvim --remote-tab-silent"
 alias vib="mvim --remote-silent"
 alias start_mongodb="mongod --config /usr/local/etc/mongod.conf"
 alias start_postgres="postgres -D /usr/local/var/postgres"
+alias stop_postgres="pg_ctl -D /usr/local/var/postgres stop"
 alias start_virtuoso="cd /usr/local/Cellar/virtuoso/7.0.0/var/lib/virtuoso/db & virtuoso-t -f"
 
 
@@ -51,4 +56,13 @@ function generate_random
   else
     env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c $argv
   end
+end
+
+function preview_markdown
+  # Start gfm_live_preview (in the background) and wait until its up
+  # then launch preview in webbrowser
+  gfm_live_preview $argv &
+  while open http://localhost:31337 | not nc localhost 31337; sleep 1; end
+  # Bring Sinatra back to the foreground
+  while [ 1 ]; fg 2> /dev/null; [ $status ]; break; end
 end
