ec82c56d8f6773a02e6a465d19cec38b9a3b565a
Chris Seelus <chris@imeos.com>
2017-03-26 20:23:57 +0200

    Fish: yarn integration and minor refactorings

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -5,21 +5,20 @@ set -U EDITOR nvim
 set default_path /usr/bin /usr/sbin /bin /sbin
 set homebrew /usr/local/bin /usr/local/sbin
 # set latex "/usr/texbin"
-set odbcsys "/etc"
-set odbc "/usr/local/etc"
-set brew_rbenv "/usr/local/var/rbenv/shims"
+set brew_rbenv /usr/local/var/rbenv/shims
+set yarn ~/.config/yarn/global/node_modules/.bin
 # PATH ($default should be last)
-set -gx PATH $homebrew $latex $odbcsys $odbc $brew_rbenv $default_path
+set -gx PATH $homebrew $brew_rbenv $yarn $default_path
 # No more bundle exec
 # set -gx  RUBYGEMS_GEMDEPS -
 
-# Ruby (rbenv)
+# Change rbenv root path
 set -gx RBENV_ROOT /usr/local/var/rbenv
 
 # Aliases
 alias bex="bundle exec"
 alias cpwd="pwd | tr -d '\n' | pbcopy"
-alias folder_size='du -sh */'
+alias folder_size='du -hd1'
 alias hostsconfig="sudo nvim /etc/hosts"
 alias vhostsconfig="sudo nvim /etc/apache2/extra/httpd-vhosts.conf"
 alias fishconfig="nvim ~/.config/fish/config.fish"
@@ -29,7 +28,7 @@ alias preview_markdown="markdown-preview"
 alias start_mongodb="mongod --config /usr/local/etc/mongod.conf"
 alias start_postgres="postgres -D /usr/local/var/postgres"
 alias stop_postgres="pg_ctl -D /usr/local/var/postgres stop"
-alias vim="nvim"
+# alias vim="nvim"
 
 
 # Functions
@@ -42,4 +41,5 @@ function generate_random
   end
 end
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+# iTerm integration
+# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
